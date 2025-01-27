b5f25118a9dcd2035a5c31bf8a1d4f242c42f7b0
jy2004 <UserCheng24@163.com>
2025-01-27 19:50:56 +0800

    测试2

---
+++ 测试2.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: jy2004 UserCheng24@163.com
+ * @Date: 2025-01-27 19:49:48
+ * @LastEditors: jy2004 UserCheng24@163.com
+ * @LastEditTime: 2025-01-27 19:50:36
+ * @FilePath: \Algorithmic punch\测试2.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 测试2
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
